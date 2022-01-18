47a718bfef23adccf729bf60b4437982ab7efe95
Ben Gowers <beng@theodo.co.uk>
2022-01-18 10:56:13 +0000

    feat: add links

--- pages/[ditto].tsx
+++ pages/[ditto].tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import Link from "next/link";
 
 const Ditto = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <span> {count} </span>
-      <button onClick={() => setCount((x) => x + 1)}>Click me</button>
-    </div>
+    <>
+      <div>
+        <span> {count} </span>
+        <button onClick={() => setCount((x) => x + 1)}>Click me</button>
+      </div>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+    </>
   );
 };
 
